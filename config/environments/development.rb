f1800ce604efbd1ae95eb7dd8eca111dc2423078
Eric-Guo <eric.guocz@gmail.com>
2019-09-25 22:45:14 +0800

    Try shopify app on Rails 6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << 'ericguo.ggrok.com'
+  # Used to ignore "Cannot render console from" from NGROK implementation
+  config.web_console.whiny_requests = false
 end
